f6b7d13dea47402caf180bf3cf8a4bcf6712903a
drain <root@drain.gang>
2024-09-29 13:57:12 -0400

    scare

---
+++ README.md
@@ -0,0 +1,5 @@
+# scare
+
+scare discord robot owns delta skidded discord robot - ethot / vampire boy was here
+# : )
+scare bot source is better and far more superior than marian the skid's code.
---
+++ structure/config.py
@@ -0,0 +1,50 @@
+class ShardStatus:
+    ok: int = 8650575
+    warning: int = 16765007
+    critical: int = 16711680
+
+
+class SCARE:
+    token: str = (
+        "hi"
+    )
+    verified_scare: str = (
+        "hi"
+    )
+    owners: list = [
+        326720789569339395,
+        1188955485462872226,
+        1150974001317957673,
+        915350867438338058,
+        1107903478451408936,
+        596752300756697098,
+        1074668481867419758,
+        1137846765576540171,
+        1280856653230505994
+    ]
+    workers: list = [
+        "h"
+    ]
+    captcha: str = "asdasdas"
+    proxy: str = "asdasd"
+
+
+class Database:
+    host: str = "sdfsdfsdf"
+    database: str = "sdfdsfsf"
+    port: str = "sdff"
+    user: str = "fdsfds"
+    password: str = "sdfdsf"
+
+
+class Paginator:
+    next: str = "<:emoji_9:1213578749375807498>"
+    previous: str = "<:emoji_10:1213578771450560583>"
+    cancel: str = "<:emoji_7:1213578698880589844>"
+    navigate: str = "<:emoji_8:1213578728450560101>"
+
+
+class API:
+    weather: str = ""
+    lastfm: str = ""
+    luma: str = ""
